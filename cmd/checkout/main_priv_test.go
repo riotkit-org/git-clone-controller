e55bac0efa23a56bd10ef332e228a4e13cb6d32c
John Doe <johndoe@example.com>
2022-05-29 12:11:30 +0200

    feat: Increase tests coverage

---
+++ cmd/checkout/main_priv_test.go
@@ -0,0 +1,45 @@
+package checkout
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetUrlWithCredentials(t *testing.T) {
+	c := Command{
+		Username: "riotkit",
+		Token:    "psst",
+	}
+
+	c.Url = "https://git.myexample.org/example/wordpress-theme.git"
+
+	url, err := c.getUrlWithCredentials()
+	assert.Nil(t, err)
+	assert.Equal(t, "https://riotkit:psst@git.myexample.org:443/example/wordpress-theme.git", url)
+}
+
+func TestGetUrlWithCredentials_HTTP(t *testing.T) {
+	c := Command{
+		Username: "riotkit",
+		Token:    "psst",
+	}
+
+	c.Url = "http://git.myexample.org/example/wordpress-theme.git"
+
+	url, err := c.getUrlWithCredentials()
+	assert.Nil(t, err)
+	assert.Equal(t, "http://riotkit:psst@git.myexample.org:80/example/wordpress-theme.git", url)
+}
+
+func TestGetUrlWithCredentials_GIT(t *testing.T) {
+	c := Command{
+		Username: "riotkit",
+		Token:    "psst",
+	}
+
+	c.Url = "git@github.com:riotkit-org/git-clone-operator.git"
+
+	url, err := c.getUrlWithCredentials()
+	assert.Nil(t, err)
+	assert.Equal(t, "git@github.com:riotkit-org/git-clone-operator.git", url)
+}
